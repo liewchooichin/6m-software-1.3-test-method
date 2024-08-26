c2008e8884453a9a60aa4b7adf5c222be71bd792
Liew Chooi Chin <liewchooichin@gmail.com>
2024-08-26 16:49:12 +0800

    Update q1-control-flow.js

--- assignment/q1-control-flow.js
+++ assignment/q1-control-flow.js
@@ -11,5 +11,26 @@ const env = "DEV"; // Toggle between these possible values "DEV" | "STAGE" | "PR
 let databaseCredential = "devuser:password";
 
 // Task: Add code here
+function getDatabaseCredential(env){
+    let credential = "";
+    switch(env) {
+        case "DEV": // change
+            credential = "devuser:password"
+            break;
+        case "STAGE":
+            credential = "stageuser:password";
+            break;
+        case "PROD": 
+            credential = "produser:password";
+            break;
+        default:
+            credential = databaseCredential;
+    }
+    return credential;
+}
+
+databaseCredential = getDatabaseCredential(env);
 
 console.log(`Database credential for environment ${env} is ${databaseCredential}`);
+
+module.exports = { getDatabaseCredential };
